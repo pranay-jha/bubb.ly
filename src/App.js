114f0d30fce613c8f230d1775f615c5e78f3cc5c
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-06-14 03:17:02 -0700

    playing around with tailwind (button)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './styles.css';
 
 const App = () => {
     return (
+        <>
         <h1 className="text-3xl font-bold underline">Test</h1>
+        <NewBubbleButton />
+        </>
     );
 }
 
+const NewBubbleButton = () => {
+    return (
+        <button className="w-24 h-24 rounded-full bg-blue-500 hover:bg-red-500 text-white text-5xl text-center">
+            +
+        </button>
+    )
+}
 export default App;
