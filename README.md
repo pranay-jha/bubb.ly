2cd90c4da30ff9bcf9aa7c88ee7d0222312f35f0
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-06-14 02:53:19 -0700

    create-react-app and also added tailwind

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-temp
+the greatest web app to ever grace the internet
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">Test</h1>
+    );
+}
+
+export default App;
