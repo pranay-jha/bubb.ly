16aa12ee1dc42322c32998835fa546ac90e08493
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-06-25 15:41:16 -0700

    google oauth is kinda half working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './styles.css';
+import LoginHooks from './LoginHooks';
+import LogoutHooks from './LogoutHooks';
 
 const App = () => {
     return (
         <>
             <PageBackground />
-            <GoogleSignInButton />
+            <LoginHooks />
+            <LogoutHooks />
         </>
     );
 }
@@ -19,7 +22,7 @@ const PageBackground = () => {
 
 
 
-const GoogleLogInButton = () => {
+/*const GoogleLogInButton = () => {
     return (
         <button type="button" className="text-white bg-[#62B6CB] hover:bg-[#62B6CB]/90 focus:ring-4 focus:outline-none focus:ring-[#62B6CB]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
             <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
@@ -28,7 +31,7 @@ const GoogleLogInButton = () => {
             Sign in with Google
         </button>
     );
-}
+}*/
 
 
 export default App;
