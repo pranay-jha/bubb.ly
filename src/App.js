13dc08f4abd6afdc5c96143e6e88f1922737607f
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-07-16 18:29:43 -0700

    hopefully this works for u

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
-import GoogleLogout from 'react-google-login';
 import './styles.css';
+import {useEffect, useState} from 'react';
+import jwt_decode from 'jwt-decode';
 
 const App = () => {
+
+    const [user, setUser] = useState({});
+
+    function handleCallbackResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+        var userObject = jwt_decode(response.credential);
+        console.log(userObject);
+        setUser(userObject);
+        document.getElementById("signInDiv").hidden = true;
+    }
+
+    useEffect(() => {
+        /*global google*/
+        google.accounts.id.initialize({
+            client_id: "145385750304-g53v1ihlt48gu2h7p0nvam0lfqrj1aog.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        })
+
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            {theme: "outline", size: "large"}   
+        );
+
+        google.accounts.id.prompt();
+    }, []);
+
+    // no user, show sign in button
+    // if we have a user, show sign out button
+
     return (
         <>
             <body className={'h-screen w-screen bg-[#1B4965]'} onClick={() => {console.log('clicked')}}>
@@ -15,58 +44,17 @@ const App = () => {
 
 const ButtonBar = () => {
     return (
-        <div className={'w-screen h-15 absolute top-0 right-0 left-0 bg-[#102C3C] items-center'}>
-            <GoogleLogInButton />
-            <GoogleLogOutButton />
+        <div className={'w-screen h-50 absolute top-0 right-0 left-0 bg-[#102C3C] flex justify-center items-center'}>
+            <GoogleButton/>
         </div>
     )
 }
 
-const responseGoogle = (response) => {
-  console.log(response);
-}
-
-const GoogleLogInButton = () => {
+const GoogleButton = () => {
     return (
-        <GoogleLogin 
-            clientId="145385750304-g53v1ihlt48gu2h7p0nvam0lfqrj1aog.apps.googleusercontent.com"
-            render={renderProps => (
-                <button onClick={renderProps.onClick} disabled={renderProps.disabled} type="button" className="text-white bg-[#62B6CB] hover:bg-[#62B6CB]/90 focus:ring-4 focus:outline-none focus:ring-[#62B6CB]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
-                    <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                        <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
-                    </svg>
-                    Sign in with Google
-                </button>
-            )}
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            isSignedIn={true}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-        />
-    );
-}
-
-const logout = () => {
-  console.log('logout') // eslint-disable-line
-}
-
-const GoogleLogOutButton = () => {
-    return (
-        <GoogleLogout
-            clientId="145385750304-g53v1ihlt48gu2h7p0nvam0lfqrj1aog.apps.googleusercontent.com"
-            render={renderProps => (
-                <button onClick={renderProps.onClick} disabled={renderProps.disabled} type="button" className="text-white bg-[#62B6CB] hover:bg-[#62B6CB]/90 focus:ring-4 focus:outline-none focus:ring-[#62B6CB]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
-                    <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-                        <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
-                    </svg>
-                    Logout
-                </button>
-            )}
-            buttonText="Logout"
-            onLogoutSuccess={logout}
-        />
-    );
+        <div id="signInDiv">
+        </div>
+    )
 }
 
 export default App;
